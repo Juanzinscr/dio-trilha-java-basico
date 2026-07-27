36a99269b9e6cc651eb5f4c748c4d6122e32d3cc
Juanzincr <jlira1655@gmail.com>
2026-07-27 17:54:31 -0300

    criação dos "ToDo"

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela classe scanner os valores digitados no terminal
+
+        //Exibir a mensagem Conta Criada
     }   
 }
